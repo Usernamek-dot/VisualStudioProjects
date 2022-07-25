7752b82c7e15b2a7b59bd1aa0c67a8a7017df5c1
Usernamek-dot <66336947+Usernamek-dot@users.noreply.github.com>
2022-07-25 07:35:38 -0500

    Nomina Mensual project

---
+++ NominaMensual/NominaMensual/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace NominaMensual
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.ColumnaNombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnaSalarioBasicoM = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnaCategoria1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnaCategoria2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnaCategoria3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnaTotalPuntos = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnaValorBonificacion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnaSalarioDevengado = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnaDeduccionNomina = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnaSalarioNeto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnIngresarDatos = new System.Windows.Forms.Button();
+            this.btnCalcularResultados = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnReiniciar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnaNombre,
+            this.ColumnaSalarioBasicoM,
+            this.ColumnaCategoria1,
+            this.ColumnaCategoria2,
+            this.ColumnaCategoria3,
+            this.ColumnaTotalPuntos,
+            this.ColumnaValorBonificacion,
+            this.ColumnaSalarioDevengado,
+            this.ColumnaDeduccionNomina,
+            this.ColumnaSalarioNeto});
+            this.dataGridView.Location = new System.Drawing.Point(61, 12);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.Size = new System.Drawing.Size(1045, 332);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // ColumnaNombre
+            // 
+            this.ColumnaNombre.HeaderText = "Nombre";
+            this.ColumnaNombre.Name = "ColumnaNombre";
+            // 
+            // ColumnaSalarioBasicoM
+            // 
+            this.ColumnaSalarioBasicoM.HeaderText = "S B M";
+            this.ColumnaSalarioBasicoM.Name = "ColumnaSalarioBasicoM";
+            // 
+            // ColumnaCategoria1
+            // 
+            this.ColumnaCategoria1.HeaderText = "Categoria1";
+            this.ColumnaCategoria1.Name = "ColumnaCategoria1";
+            // 
+            // ColumnaCategoria2
+            // 
+            this.ColumnaCategoria2.HeaderText = "Categoria2";
+            this.ColumnaCategoria2.Name = "ColumnaCategoria2";
+            // 
+            // ColumnaCategoria3
+            // 
+            this.ColumnaCategoria3.HeaderText = "Categoria3";
+            this.ColumnaCategoria3.Name = "ColumnaCategoria3";
+            // 
+            // ColumnaTotalPuntos
+            // 
+            this.ColumnaTotalPuntos.HeaderText = "Total Puntos";
+            this.ColumnaTotalPuntos.Name = "ColumnaTotalPuntos";
+            // 
+            // ColumnaValorBonificacion
+            // 
+            this.ColumnaValorBonificacion.HeaderText = "Valor Bonificacion";
+            this.ColumnaValorBonificacion.Name = "ColumnaValorBonificacion";
+            // 
+            // ColumnaSalarioDevengado
+            // 
+            this.ColumnaSalarioDevengado.HeaderText = "Salario Devengado";
+            this.ColumnaSalarioDevengado.Name = "ColumnaSalarioDevengado";
+            // 
+            // ColumnaDeduccionNomina
+            // 
+            this.ColumnaDeduccionNomina.HeaderText = "Deduccion de Nomina";
+            this.ColumnaDeduccionNomina.Name = "ColumnaDeduccionNomina";
+            // 
+            // ColumnaSalarioNeto
+            // 
+            this.ColumnaSalarioNeto.HeaderText = "Salario Neto";
+            this.ColumnaSalarioNeto.Name = "ColumnaSalarioNeto";
+            // 
+            // btnIngresarDatos
+            // 
+            this.btnIngresarDatos.Location = new System.Drawing.Point(119, 379);
+            this.btnIngresarDatos.Name = "btnIngresarDatos";
+            this.btnIngresarDatos.Size = new System.Drawing.Size(86, 45);
+            this.btnIngresarDatos.TabIndex = 1;
+            this.btnIngresarDatos.Text = "Ingresar Datos";
+            this.btnIngresarDatos.UseVisualStyleBackColor = true;
+            this.btnIngresarDatos.Click += new System.EventHandler(this.btnIngresarDatos_Click);
+            // 
+            // btnCalcularResultados
+            // 
+            this.btnCalcularResultados.Location = new System.Drawing.Point(362, 377);
+            this.btnCalcularResultados.Name = "btnCalcularResultados";
+            this.btnCalcularResultados.Size = new System.Drawing.Size(80, 48);
+            this.btnCalcularResultados.TabIndex = 2;
+            this.btnCalcularResultados.Text = "Calcular Resultados";
+            this.btnCalcularResultados.UseVisualStyleBackColor = true;
+            this.btnCalcularResultados.Click += new System.EventHandler(this.btnCalcularResultados_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(932, 379);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(85, 48);
+            this.btnSalir.TabIndex = 3;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnReiniciar
+            // 
+            this.btnReiniciar.Location = new System.Drawing.Point(584, 379);
+            this.btnReiniciar.Name = "btnReiniciar";
+            this.btnReiniciar.Size = new System.Drawing.Size(101, 49);
+            this.btnReiniciar.TabIndex = 4;
+            this.btnReiniciar.Text = "Reiniciar";
+            this.btnReiniciar.UseVisualStyleBackColor = true;
+            this.btnReiniciar.Click += new System.EventHandler(this.btnReiniciar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1204, 450);
+            this.Controls.Add(this.btnReiniciar);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnCalcularResultados);
+            this.Controls.Add(this.btnIngresarDatos);
+            this.Controls.Add(this.dataGridView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button btnIngresarDatos;
+        private System.Windows.Forms.Button btnCalcularResultados;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnaNombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnaSalarioBasicoM;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnaCategoria1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnaCategoria2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnaCategoria3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnaTotalPuntos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnaValorBonificacion;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnaSalarioDevengado;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnaDeduccionNomina;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnaSalarioNeto;
+        private System.Windows.Forms.Button btnReiniciar;
+    }
+}
+
---
+++ NominaMensual/NominaMensual/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace NominaMensual
+{
+    public partial class Form1 : Form
+    {
+        //Variables
+        public int  Vendedores = 3;
+        public int Filas;
+        public double[,] MatrizNomina;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIngresarDatos_Click(object sender, EventArgs e)
+        {
+            try {
+                //crear celdas
+                dataGridView.RowCount = (Vendedores);
+                //crear celdas de resultados
+                dataGridView.Rows[Vendedores].Cells[0].Value = "Valor total";
+                dataGridView.Rows[Vendedores + 1].Cells[0].Value = "Valor promedio";
+                dataGridView.Rows[Vendedores + 2].Cells[0].Value = "Valor mayor";
+                dataGridView.Rows[Vendedores + 3].Cells[0].Value = "Valor menor";
+
+                //darle tamaño a la matriz   filas           colum
+                MatrizNomina = new Double[ (Vendedores + 4 ), 10 ];
+
+                //Vectores
+                string[] VNombre = new String[Vendedores];
+                double[] VBonificacion = new Double[Vendedores];
+
+                //asignar las celdas
+                for ( Filas = 0; Filas < Vendedores; Filas++) {
+
+                    
+                    VNombre[Filas] = Convert.ToString(Interaction.inputBox("Ingrese el Nombre del Vendedor"));
+                    dataGridView.Rows[Filas].Cells[0].Value = VNombre[Filas];
+
+                    //crear input
+                    MatrizNomina[Filas, 0] = Convert.ToDouble(Interaction.inputBox("Ingrese el salario basico mensual"));
+                    //agregar a datagridview
+                    dataGridView.Rows[Filas].Cells[1].Value = String.Format("{0:#,0.0}", MatrizNomina[Filas, 0]);
+
+                    //crear input
+                    MatrizNomina[Filas, 1] = Convert.ToDouble(Interaction.inputBox("Ingrese la cantidad de tiquetes de la categoria 1"));
+                    //agregar a datagridview
+                    dataGridView.Rows[Filas].Cells[2].Value = String.Format("{0:#,0.0}", MatrizNomina[Filas, 1]);
+
+
+                    //crear input
+                    MatrizNomina[Filas, 2] = Convert.ToDouble(Interaction.inputBox("Ingrese la cantidad de tiquetes de la categoria 2"));
+                    //agregar a datagridview
+                    dataGridView.Rows[Filas].Cells[3].Value = String.Format("{0:#,0.0}", MatrizNomina[Filas, 2]);
+
+                    //crear input
+                    MatrizNomina[Filas, 3] = Convert.ToDouble(Interaction.inputBox("Ingrese la cantidad de tiquetes de la categoria 3"));
+                    //agregar a datagridview
+                    dataGridView.Rows[Filas].Cells[4].Value = String.Format("{0:#,0.0}", MatrizNomina[Filas, 3]);
+
+                    //TOTAL PUNTOS
+                    MatrizNomina[Filas, 4] = ((MatrizNomina[Filas,1]*5)+(MatrizNomina[Filas, 1] * 7)+(MatrizNomina[Filas, 1] * 10));
+                    dataGridView.Rows[Filas].Cells[5].Value = String.Format("{0:#,0.00}", MatrizNomina[Filas, 4]);
+
+                    //VALOR BONIFICACION
+
+                    // 0 a 10.000 
+                    if (MatrizNomina[Filas, 4] <= 10000) {
+
+                        VBonificacion[Filas] = (MatrizNomina[Filas, 0] * 0.10);
+                    }
+                    // 10.001 a 50.000 
+                    else if (MatrizNomina[Filas, 4] >= 10001 && MatrizNomina[Filas, 4] <= 50000) {
+
+                        VBonificacion[Filas] = (MatrizNomina[Filas, 0] * 0.12);
+
+                    }
+                    // 50.001 a 100.000
+                    else if (MatrizNomina[Filas, 4] >= 50001 && MatrizNomina[Filas, 4] <= 100000)
+                    {
+
+                        VBonificacion[Filas] = (MatrizNomina[Filas, 0] * 0.15);
+
+                    }
+                    // 100.001 puntos en adelante
+                    else 
+                    {
+
+                        VBonificacion[Filas] = (MatrizNomina[Filas , 0] * 0.20);
+
+                    }
+
+                    dataGridView.Rows[Filas].Cells[6].Value = VBonificacion[Filas];
+
+
+
+                    //SALARIO DEVENGADO
+                    MatrizNomina[Filas, 6] = (MatrizNomina[Filas, 0] +  VBonificacion[Filas]);
+                    dataGridView.Rows[Filas].Cells[7].Value = String.Format("{0:#,0.00}", MatrizNomina[Filas, 6]);
+
+                    //DEDUCCION NOMINA
+                    MatrizNomina[Filas, 7] = (MatrizNomina[Filas, 6] / 0.084);
+                    dataGridView.Rows[Filas].Cells[8].Value = String.Format("{0:#,0.00}", MatrizNomina[Filas, 7]);
+
+
+                    //SALARIO NETO
+                    MatrizNomina[Filas, 8] = (MatrizNomina[Filas, 6] - MatrizNomina[Filas, 7]);
+                    dataGridView.Rows[Filas].Cells[9].Value = String.Format("{0:#,0.00}", MatrizNomina[Filas, 8]);
+
+
+
+                    btnCalcularResultados.Enabled = true;
+                    btnIngresarDatos.Enabled = false;
+
+                }
+
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Error al ingresar datos, favor verificar");
+                dataGridView.RowCount = 0;
+            }
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            DialogResult Salir = MessageBox.Show("Desea Salir (Si/No) ?", "Salir Aplicación", MessageBoxButtons.YesNo);
+            if (Salir == DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+
+        private void btnCalcularResultados_Click(object sender, EventArgs e)
+        {
+            int columnas;
+            Double acumulador = 0,  ValorMenor = 0, ValorMayor = 0;
+
+            for (columnas=0; columnas <= 9; columnas++) {
+
+                for (Filas=0; Filas< Vendedores; Filas++) {
+                    //contador donde se van sumando varios resultados
+                    acumulador = acumulador + MatrizNomina[Filas,columnas];
+
+                    /* MENOR Y MAYOR
+                    comparar un resultado con el otro*/
+                    if (Filas == 0)
+                    {
+
+                        ValorMayor = MatrizNomina[Filas, columnas];
+                        ValorMenor = MatrizNomina[Filas, columnas];
+                        
+;
+                    }
+                    else {
+                        if (MatrizNomina[Filas, columnas] > ValorMayor)
+                        {
+                            ValorMayor = MatrizNomina[Filas, columnas];
+                        }
+                        else {
+                            if (MatrizNomina[Filas, columnas] < ValorMenor) {
+                                ValorMenor = MatrizNomina[Filas, columnas];
+                            }
+                        }
+                    }
+
+                }
+
+                //mayor
+                MatrizNomina[(Vendedores + 1), columnas] = ValorMayor;
+                dataGridView.Rows[Vendedores + 1].Cells[columnas + 1].Value = String.Format("{0:#,0.00}", MatrizNomina[(Vendedores + 1), columnas]);
+
+                //menor
+                MatrizNomina[(Vendedores + 2), columnas] = ValorMenor;
+                dataGridView.Rows[Vendedores + 2].Cells[columnas + 1].Value = String.Format("{0:#,0.00}", MatrizNomina[(Vendedores + 2), columnas]);
+
+
+                /* VALOR TOTAL Y PROMEDIO*/
+
+                //TOTAL
+                MatrizNomina[Vendedores, columnas] = acumulador ;
+                dataGridView.Rows[Vendedores].Cells[columnas + 1].Value = String.Format("{0:#,0.00}" , MatrizNomina[Vendedores,columnas]);
+
+
+                //promedio
+                MatrizNomina[Vendedores, columnas] = acumulador /  3 ;
+                dataGridView.Rows[Vendedores].Cells[columnas + 1].Value = String.Format("{0:#,0.00}", MatrizNomina[Vendedores, columnas]);
+
+               
+                acumulador = 0;
+
+            }
+        }
+
+        private void btnReiniciar_Click(object sender, EventArgs e)
+        {
+            dataGridView.RowCount = 0;
+            btnReiniciar.Enabled = false;
+            btnIngresarDatos.Enabled = true;
+
+
+        }
+    }
+
+    internal class Interaction
+    {
+        internal static object inputBox(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
