f612031f32f734782893054ecdb2073ee6671aed
Usernamek-dot <66336947+Usernamek-dot@users.noreply.github.com>
2022-07-25 07:34:06 -0500

    Matriz Notas Project

---
+++ MatrizNotas/MatrizNotas/frmNotas.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace MatrizNotas
+{
+    partial class frmNotas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dvgNotas = new System.Windows.Forms.DataGridView();
+            this.btnIngresarDatos = new System.Windows.Forms.Button();
+            this.btnCalcularResultados = new System.Windows.Forms.Button();
+            this.btnReiniciarProceso = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.clmNombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmNota1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmNota2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmNota3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmNota4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmNota5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmNotaDef = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmResultado = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dvgNotas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dvgNotas
+            // 
+            this.dvgNotas.AllowUserToAddRows = false;
+            this.dvgNotas.AllowUserToDeleteRows = false;
+            this.dvgNotas.BackgroundColor = System.Drawing.SystemColors.ControlLight;
+            this.dvgNotas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dvgNotas.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.clmNombre,
+            this.clmNota1,
+            this.clmNota2,
+            this.clmNota3,
+            this.clmNota4,
+            this.clmNota5,
+            this.clmNotaDef,
+            this.clmResultado});
+            this.dvgNotas.GridColor = System.Drawing.SystemColors.Info;
+            this.dvgNotas.Location = new System.Drawing.Point(4, 12);
+            this.dvgNotas.Name = "dvgNotas";
+            this.dvgNotas.ReadOnly = true;
+            this.dvgNotas.RowTemplate.Height = 24;
+            this.dvgNotas.Size = new System.Drawing.Size(784, 312);
+            this.dvgNotas.TabIndex = 0;
+            this.dvgNotas.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dvgNotas_CellContentClick);
+            // 
+            // btnIngresarDatos
+            // 
+            this.btnIngresarDatos.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.btnIngresarDatos.Location = new System.Drawing.Point(122, 356);
+            this.btnIngresarDatos.Name = "btnIngresarDatos";
+            this.btnIngresarDatos.Size = new System.Drawing.Size(75, 50);
+            this.btnIngresarDatos.TabIndex = 1;
+            this.btnIngresarDatos.Text = "Ingresar Datos";
+            this.btnIngresarDatos.UseVisualStyleBackColor = false;
+            this.btnIngresarDatos.Click += new System.EventHandler(this.btnIngresarDatos_Click);
+            // 
+            // btnCalcularResultados
+            // 
+            this.btnCalcularResultados.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.btnCalcularResultados.Enabled = false;
+            this.btnCalcularResultados.Location = new System.Drawing.Point(278, 351);
+            this.btnCalcularResultados.Name = "btnCalcularResultados";
+            this.btnCalcularResultados.Size = new System.Drawing.Size(94, 60);
+            this.btnCalcularResultados.TabIndex = 2;
+            this.btnCalcularResultados.Text = "Calcular Resultados";
+            this.btnCalcularResultados.UseVisualStyleBackColor = false;
+            this.btnCalcularResultados.Click += new System.EventHandler(this.btnCalcularResultados_Click);
+            // 
+            // btnReiniciarProceso
+            // 
+            this.btnReiniciarProceso.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.btnReiniciarProceso.Enabled = false;
+            this.btnReiniciarProceso.Location = new System.Drawing.Point(438, 351);
+            this.btnReiniciarProceso.Name = "btnReiniciarProceso";
+            this.btnReiniciarProceso.Size = new System.Drawing.Size(96, 60);
+            this.btnReiniciarProceso.TabIndex = 3;
+            this.btnReiniciarProceso.Text = "Reiniciar Proceso";
+            this.btnReiniciarProceso.UseVisualStyleBackColor = false;
+            this.btnReiniciarProceso.Click += new System.EventHandler(this.btnReiniciarProceso_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.btnSalir.Location = new System.Drawing.Point(596, 356);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 50);
+            this.btnSalir.TabIndex = 4;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // clmNombre
+            // 
+            this.clmNombre.HeaderText = "Nombre";
+            this.clmNombre.Name = "clmNombre";
+            this.clmNombre.ReadOnly = true;
+            this.clmNombre.Width = 80;
+            // 
+            // clmNota1
+            // 
+            this.clmNota1.HeaderText = "Nota1";
+            this.clmNota1.Name = "clmNota1";
+            this.clmNota1.ReadOnly = true;
+            this.clmNota1.Width = 70;
+            // 
+            // clmNota2
+            // 
+            this.clmNota2.HeaderText = "Nota2";
+            this.clmNota2.Name = "clmNota2";
+            this.clmNota2.ReadOnly = true;
+            this.clmNota2.Width = 70;
+            // 
+            // clmNota3
+            // 
+            this.clmNota3.HeaderText = "Nota3";
+            this.clmNota3.Name = "clmNota3";
+            this.clmNota3.ReadOnly = true;
+            this.clmNota3.Width = 70;
+            // 
+            // clmNota4
+            // 
+            this.clmNota4.HeaderText = "Nota4";
+            this.clmNota4.Name = "clmNota4";
+            this.clmNota4.ReadOnly = true;
+            this.clmNota4.Width = 70;
+            // 
+            // clmNota5
+            // 
+            this.clmNota5.HeaderText = "Nota5";
+            this.clmNota5.Name = "clmNota5";
+            this.clmNota5.ReadOnly = true;
+            this.clmNota5.Width = 70;
+            // 
+            // clmNotaDef
+            // 
+            this.clmNotaDef.HeaderText = "Nota Definitiva";
+            this.clmNotaDef.Name = "clmNotaDef";
+            this.clmNotaDef.ReadOnly = true;
+            // 
+            // clmResultado
+            // 
+            this.clmResultado.HeaderText = "Resultado";
+            this.clmResultado.Name = "clmResultado";
+            this.clmResultado.ReadOnly = true;
+            // 
+            // frmNotas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnReiniciarProceso);
+            this.Controls.Add(this.btnCalcularResultados);
+            this.Controls.Add(this.btnIngresarDatos);
+            this.Controls.Add(this.dvgNotas);
+            this.Name = "frmNotas";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmNotas_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dvgNotas)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dvgNotas;
+        private System.Windows.Forms.Button btnIngresarDatos;
+        private System.Windows.Forms.Button btnCalcularResultados;
+        private System.Windows.Forms.Button btnReiniciarProceso;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmNombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmNota1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmNota2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmNota3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmNota4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmNota5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmNotaDef;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmResultado;
+    }
+}
+
---
+++ MatrizNotas/MatrizNotas/frmNotas.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace MatrizNotas
+{
+    public partial class frmNotas : Form
+    {
+
+        public int N, F;
+        public Double[,] MatrizNotas ;
+            
+
+        public frmNotas()
+        {
+            InitializeComponent();
+        }
+
+        private void frmNotas_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnIngresarDatos_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                N = Convert.ToInt32(Interaction.InputBox("Ingrese el número de Estudiantes"));
+
+                dvgNotas.RowCount = (N + 3);
+                dvgNotas.Rows[N + 1].Cells[0].Value = "Nota Mayor";
+                dvgNotas.Rows[N + 2].Cells[0].Value = "Nota Menor";
+                dvgNotas.Rows[N].Cells[7].Style.BackColor = Color.Transparent;
+                dvgNotas.Rows[N + 1].Cells[7].Style.BackColor = Color.Transparent;
+                dvgNotas.Rows[N + 2].Cells[7].Style.BackColor = Color.Transparent;
+                dvgNotas.Rows[N].Cells[0].Value = "Nota Promedio";
+
+
+
+                /*Asignar valor a la matriz**/
+                MatrizNotas = new double[(N + 3), 6];
+
+
+                String[] VectorNombre = new string[N];
+                String[] VectorResultado = new string[N];
+
+
+                /* Ciclo para asignar las columnas al datagridview**/
+                for (F = 0; F < N; F++)
+                {
+
+                    /**Se asigna al la variable del ciclo un celda interactiva*/
+                    VectorNombre[F] = Interaction.InputBox("Ingrese nombre del estudiante ");
+                    ////Asignar el valor a la celda del data datagridview
+                    dvgNotas.Rows[F].Cells[0].Value = VectorNombre[F];
+
+                    /**Se asigna al la variable del ciclo un celda interactiva*/
+                    MatrizNotas[F, 0] = Convert.ToDouble(Interaction.InputBox("Ingrese la nota 1"));
+                    ////Asignar el valor a la celda del data datagridview
+                    dvgNotas.Rows[F].Cells[1].Value = MatrizNotas[F, 0];
+
+                    /**Se asigna al la variable del ciclo un celda interactiva*/
+                    MatrizNotas[F, 1] = Convert.ToDouble(Interaction.InputBox("Ingrese la nota 2"));
+                    ////Asignar el valor a la celda del data datagridview
+                    dvgNotas.Rows[F].Cells[2].Value = MatrizNotas[F, 1];
+
+                    /**Se asigna al la variable del ciclo un celda interactiva*/
+                    MatrizNotas[F, 2] = Convert.ToDouble(Interaction.InputBox("Ingrese la nota 3"));
+                    ////Asignar el valor a la celda del data datagridview
+                    dvgNotas.Rows[F].Cells[3].Value = MatrizNotas[F, 2];
+
+                    /**Se asigna al la variable del ciclo un celda interactiva*/
+                    MatrizNotas[F, 3] = Convert.ToDouble(Interaction.InputBox("Ingrese la nota 4"));
+                    ////Asignar el valor a la celda del data datagridview
+                    dvgNotas.Rows[F].Cells[4].Value = MatrizNotas[F, 3];
+
+                    /**Se asigna al la variable del ciclo un celda interactiva*/
+                    MatrizNotas[F, 4] = Convert.ToDouble(Interaction.InputBox("Ingrese la nota 5"));
+                    ////Asignar el valor a la celda del data datagridview
+                    dvgNotas.Rows[F].Cells[5].Value = MatrizNotas[F, 4];
+
+
+                
+                    //calcular nota definitiva
+                    MatrizNotas[F, 5] = (((MatrizNotas[F, 0] + MatrizNotas[F, 1] + MatrizNotas[F, 2]) / 3) * 0.4) + (MatrizNotas[F, 3] * 0.25) + (MatrizNotas[F, 4] * 0.35);
+                    dvgNotas.Rows[F].Cells[6].Value = String.Format("{0:#,0.00}",MatrizNotas[F, 5]);
+
+
+                    if (MatrizNotas[F, 5] >= 3.0) {
+                        VectorResultado[F] = "GANA";
+                        dvgNotas.Rows[F].Cells[7].Style.BackColor = Color.Blue;
+                    }
+
+
+                    else {
+                        VectorResultado[F] = "PIERDE";
+                        dvgNotas.Rows[F].Cells[7].Style.BackColor = Color.Red;
+                    }
+                    dvgNotas.Rows[F].Cells[7].Value = VectorResultado[F];
+
+
+                    btnCalcularResultados.Enabled = true;
+                    btnIngresarDatos.Enabled = false;
+
+
+                }
+
+
+                
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Error al ingresar datos,Intente de nuevo ");
+                dvgNotas.RowCount = 0;
+
+            }
+
+        }
+
+        private void btnCalcularResultados_Click(object sender, EventArgs e)
+        {
+
+          
+                //las variables se inciializan como privadas
+            int Columnas;
+            double Acumulador = 0, NotaMayor = 0, NotaMenor = 0;
+
+            for (Columnas = 0; Columnas <= 5; Columnas++) {
+                for(F = 0; F<N; F++)
+                {
+                    Acumulador = Acumulador + MatrizNotas[F,Columnas];
+                    if (F == 0)
+                    {
+                        NotaMayor = MatrizNotas[F, Columnas];
+                        NotaMenor = MatrizNotas[F, Columnas];
+                    }
+                    else {
+                        if (MatrizNotas[F, Columnas] > NotaMayor) {
+                            NotaMayor = MatrizNotas[F, Columnas];
+                        } else {
+
+                            if (MatrizNotas[F, Columnas] < NotaMenor) {
+                                NotaMenor = MatrizNotas[F, Columnas];
+                            }
+                        }
+                    }
+                }
+
+                MatrizNotas[N, Columnas] = Acumulador / N;
+
+                //MAYOR
+                dvgNotas.Rows[N].Cells[Columnas+1].Value = String.Format("{0:#,0.00}" , MatrizNotas[N, Columnas]);
+                MatrizNotas[(N + 1), Columnas] = NotaMayor;
+
+                //MENOR
+                dvgNotas.Rows[N+1].Cells[Columnas + 1].Value = String.Format("{0:#,0.00}" , MatrizNotas[(N + 1), Columnas]);
+                MatrizNotas[(N + 2), Columnas] = NotaMenor;
+
+                dvgNotas.Rows[N + 2].Cells[Columnas + 1].Value = String.Format("{0:#,0.00}", MatrizNotas[(N + 2), Columnas]);
+                Acumulador = 0;
+
+
+            }
+
+            btnReiniciarProceso.Enabled = true;
+            btnCalcularResultados.Enabled = false;
+
+        }
+
+        private void btnReiniciarProceso_Click(object sender, EventArgs e)
+        {
+            dvgNotas.RowCount = 0;
+
+            btnIngresarDatos.Enabled = true;
+            btnReiniciarProceso.Enabled = false;
+
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            DialogResult Salir = MessageBox.Show("Desea irse del sistema ?",
+                "SALIR SISTEMA", MessageBoxButtons.YesNo);
+            if (Salir == DialogResult.Yes) {
+                Close();
+            }
+        }
+
+        private void dvgNotas_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
