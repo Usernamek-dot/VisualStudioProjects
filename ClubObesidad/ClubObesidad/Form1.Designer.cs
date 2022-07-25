1e7be42f4b568eaa9c8d4dbba49eaf32ef496e4b
Usernamek-dot <66336947+Usernamek-dot@users.noreply.github.com>
2022-07-25 07:29:36 -0500

    Peajes project
    These are files of Peajes project.

---
+++ ClubObesidad/ClubObesidad/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace ClubObesidad
+{
+    partial class frmClub
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Estatura = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Edad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PesoBascula1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PesoBascula2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PesoBascula3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PesoPromedio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.IMC = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NRC = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.EdadSupervivencia = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnCalcularResultados = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnIngresarDatos = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Nombre,
+            this.Estatura,
+            this.Edad,
+            this.PesoBascula1,
+            this.PesoBascula2,
+            this.PesoBascula3,
+            this.PesoPromedio,
+            this.IMC,
+            this.NRC,
+            this.EdadSupervivencia});
+            this.dataGridView.Location = new System.Drawing.Point(0, 12);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.Size = new System.Drawing.Size(1131, 326);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.Name = "Nombre";
+            // 
+            // Estatura
+            // 
+            this.Estatura.HeaderText = "Estatura";
+            this.Estatura.Name = "Estatura";
+            // 
+            // Edad
+            // 
+            this.Edad.HeaderText = "Edad";
+            this.Edad.Name = "Edad";
+            // 
+            // PesoBascula1
+            // 
+            this.PesoBascula1.HeaderText = "Peso Bascula1";
+            this.PesoBascula1.Name = "PesoBascula1";
+            // 
+            // PesoBascula2
+            // 
+            this.PesoBascula2.HeaderText = "Peso Bascula2";
+            this.PesoBascula2.Name = "PesoBascula2";
+            // 
+            // PesoBascula3
+            // 
+            this.PesoBascula3.HeaderText = "Peso Bascula3";
+            this.PesoBascula3.Name = "PesoBascula3";
+            // 
+            // PesoPromedio
+            // 
+            this.PesoPromedio.HeaderText = "Peso Promedio";
+            this.PesoPromedio.Name = "PesoPromedio";
+            this.PesoPromedio.Width = 150;
+            // 
+            // IMC
+            // 
+            this.IMC.HeaderText = "IMC";
+            this.IMC.Name = "IMC";
+            // 
+            // NRC
+            // 
+            this.NRC.HeaderText = "NRC";
+            this.NRC.Name = "NRC";
+            // 
+            // EdadSupervivencia
+            // 
+            this.EdadSupervivencia.HeaderText = "ES";
+            this.EdadSupervivencia.Name = "EdadSupervivencia";
+            this.EdadSupervivencia.Width = 150;
+            // 
+            // btnCalcularResultados
+            // 
+            this.btnCalcularResultados.Location = new System.Drawing.Point(505, 385);
+            this.btnCalcularResultados.Name = "btnCalcularResultados";
+            this.btnCalcularResultados.Size = new System.Drawing.Size(110, 53);
+            this.btnCalcularResultados.TabIndex = 1;
+            this.btnCalcularResultados.Text = "Resultados";
+            this.btnCalcularResultados.UseVisualStyleBackColor = true;
+            this.btnCalcularResultados.Click += new System.EventHandler(this.btnCalcularResultados_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(818, 385);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(94, 53);
+            this.btnSalir.TabIndex = 2;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnIngresarDatos
+            // 
+            this.btnIngresarDatos.Location = new System.Drawing.Point(192, 385);
+            this.btnIngresarDatos.Name = "btnIngresarDatos";
+            this.btnIngresarDatos.Size = new System.Drawing.Size(85, 53);
+            this.btnIngresarDatos.TabIndex = 3;
+            this.btnIngresarDatos.Text = "Ingresar datos";
+            this.btnIngresarDatos.UseVisualStyleBackColor = true;
+            this.btnIngresarDatos.Click += new System.EventHandler(this.btnIngresarDatos_Click);
+            // 
+            // frmClub
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1132, 450);
+            this.Controls.Add(this.btnIngresarDatos);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnCalcularResultados);
+            this.Controls.Add(this.dataGridView);
+            this.Name = "frmClub";
+            this.Text = "Averigue su peso";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button btnCalcularResultados;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnIngresarDatos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Estatura;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Edad;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PesoBascula1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PesoBascula2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PesoBascula3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PesoPromedio;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IMC;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NRC;
+        private System.Windows.Forms.DataGridViewTextBoxColumn EdadSupervivencia;
+    }
+}
+
---
+++ ClubObesidad/ClubObesidad/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClubObesidad
+{
+    public partial class frmClub : Form
+    {
+        //Crear Variables 
+        public int NumeroPersonas, Filas;
+        public Double[,] MatrizPersonas;
+
+
+        public frmClub()
+        {
+            InitializeComponent();
+        }
+        private void btnIngresarDatos_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                NumeroPersonas = Convert.ToInt32(Interaction.InputBox("Ingrese el número  de personas que aplicarán"));
+
+
+                dataGridView.RowCount = (NumeroPersonas + 4);
+                dataGridView.RowCount = (NumeroPersonas + 4);
+                dataGridView.Rows[NumeroPersonas].Cells[0].Value = "Nombre";
+
+                dataGridView.Rows[NumeroPersonas].Cells[0].Value = "Valor total";
+                dataGridView.Rows[NumeroPersonas + 1].Cells[0].Value = "Valor promedio";
+                dataGridView.Rows[NumeroPersonas + 2].Cells[0].Value = "Valor mayor ";
+                dataGridView.Rows[NumeroPersonas + 3].Cells[0].Value = "Valor menor";
+                dataGridView.Rows[NumeroPersonas].Cells[7].Style.BackColor = Color.Transparent;
+                dataGridView.Rows[NumeroPersonas + 1].Cells[7].Style.BackColor = Color.Transparent;
+                dataGridView.Rows[NumeroPersonas + 2].Cells[7].Style.BackColor = Color.Transparent;
+
+                MatrizPersonas = new Double[(NumeroPersonas + 4), 10];
+
+                //Calcular resultados
+                String[] VectorNombre = new String[NumeroPersonas];
+                String[] VectorNRC = new String[NumeroPersonas];
+                String[] VectorES = new String[NumeroPersonas];
+
+
+                for (Filas = 0; Filas < NumeroPersonas; Filas++)
+                {
+
+                    //Ingresar los datos a los inputs
+                    VectorNombre[Filas] = Convert.ToString(Interaction.InputBox("Ingrese el Nombre de la persona"));
+                    dataGridView.Rows[Filas].Cells[0].Value = VectorNombre[Filas];
+                    MatrizPersonas[Filas, 0] = Convert.ToDouble(Interaction.InputBox("Ingrese la edad"));
+                    dataGridView.Rows[Filas].Cells[1].Value = String.Format("{0:#,0.0}", MatrizPersonas[Filas, 0]);
+                    MatrizPersonas[Filas, 1] = Convert.ToDouble(Interaction.InputBox("Ingrese la estatura"));
+                    dataGridView.Rows[Filas].Cells[2].Value = String.Format("{0:#,0.0}", MatrizPersonas[Filas, 1]);
+                    MatrizPersonas[Filas, 2] = Convert.ToDouble(Interaction.InputBox("Ingrese el peso de bascula 1"));
+                    dataGridView.Rows[Filas].Cells[3].Value = String.Format("{0:#,0.0}", MatrizPersonas[Filas, 2]);
+                    MatrizPersonas[Filas, 3] = Convert.ToDouble(Interaction.InputBox("Ingrese el peso de bascula 2"));
+                    dataGridView.Rows[Filas].Cells[4].Value = String.Format("{0:#,0.0}", MatrizPersonas[Filas, 3]);
+                    MatrizPersonas[Filas, 4] = Convert.ToDouble(Interaction.InputBox("Ingrese el peso de bascula 3"));
+                    dataGridView.Rows[Filas].Cells[5].Value = String.Format("{0:#,0.0}", MatrizPersonas[Filas, 4]);
+
+                    //peso promedio
+                    MatrizPersonas[Filas, 5] = ((MatrizPersonas[Filas, 2] + MatrizPersonas[Filas, 3] + MatrizPersonas[Filas, 4]) / 3);
+                    dataGridView.Rows[Filas].Cells[6].Value = String.Format("{0:#,0.00}", MatrizPersonas[Filas, 5]);
+                    //imc
+                    MatrizPersonas[Filas, 6] = (MatrizPersonas[Filas, 5] / (MatrizPersonas[Filas, 1] * MatrizPersonas[Filas, 1]));
+                    dataGridView.Rows[Filas].Cells[7].Value = String.Format("{0:#,0.00}", MatrizPersonas[Filas, 6]);
+
+                    //nrc
+
+                    if (MatrizPersonas[Filas, 7] < 25)
+                    {
+                        VectorNRC[Filas] = "0";
+                        dataGridView.Rows[Filas].Cells[8].Style.BackColor = Color.GreenYellow;
+                    }
+                    else if (MatrizPersonas[Filas, 7] >= 25 && MatrizPersonas[Filas, 7] <= 28)
+                    {
+                        VectorNRC[Filas] = "1";
+                        dataGridView.Rows[Filas].Cells[9].Style.BackColor = Color.GreenYellow;
+                    }
+                    else if (MatrizPersonas[Filas, 7] >= 29 && MatrizPersonas[Filas, 7] <= 33)
+                    {
+                        VectorNRC[Filas] = "2";
+                        dataGridView.Rows[Filas].Cells[10].Style.BackColor = Color.GreenYellow;
+                    }
+                    else if (MatrizPersonas[Filas, 7] >= 34 && MatrizPersonas[Filas, 7] <= 40)
+                    {
+                        VectorNRC[Filas] = "3";
+                        dataGridView.Rows[Filas].Cells[11].Style.BackColor = Color.GreenYellow;
+                    }
+                    else
+                    {
+                        VectorNRC[Filas] = "4";
+                        dataGridView.Rows[Filas].Cells[12].Style.BackColor = Color.GreenYellow;
+                    }
+
+                    //es
+
+                    if (MatrizPersonas[Filas, 0] < 50 && MatrizPersonas[Filas, 7] < 2)
+                    {
+                        VectorES[Filas] = "80";
+
+                    }
+                    else if (MatrizPersonas[Filas, 0] < 50 && MatrizPersonas[Filas, 7] >= 2)
+                    {
+                        VectorES[Filas] = "70";
+
+                    }
+                    else if (MatrizPersonas[Filas, 0] >= 50 && MatrizPersonas[Filas, 7] < 3)
+                    {
+                        VectorES[Filas] = "65";
+
+                    }
+                    else if (MatrizPersonas[Filas, 0] >= 50 && MatrizPersonas[Filas, 7] >= 3)
+                    {
+                        VectorES[Filas] = "60";
+
+                    }
+
+                    dataGridView.Rows[Filas].Cells[9].Value = VectorNRC[Filas];
+                    btnCalcularResultados.Enabled = true;
+                    btnIngresarDatos.Enabled = false;
+                }
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Error al ingresar datos, favor verificar");
+                dataGridView.RowCount = 0;
+            }
+
+        }
+
+        private void btnCalcularResultados_Click(object sender, EventArgs e)
+        {
+
+            int Columnas;
+            Double Acumulador = 0, ValorMayor = 0, ValorMenor = 0;
+            for (Columnas = 0; Columnas <= 5; Columnas ++)
+            {
+                for (Filas = 0; Filas < NumeroPersonas; Filas ++)
+                {
+                    Acumulador = Acumulador + MatrizPersonas[Filas, Columnas];
+                    if (Filas == 0)
+                    {
+                        ValorMayor = MatrizPersonas[Filas, Columnas];
+                        ValorMenor = MatrizPersonas[Filas, Columnas];
+                    }
+                    else
+                    {
+                        if (MatrizPersonas[Filas, Columnas] > ValorMayor)
+                        {
+                            ValorMayor = MatrizPersonas[Filas, Columnas];
+            }
+                        else
+                        {
+                            if (MatrizPersonas[Filas, Columnas] < ValorMenor)
+                            {
+                                ValorMenor = MatrizPersonas[Filas, Columnas];
+                            }
+                        }
+                    }
+                }
+                MatrizPersonas[NumeroPersonas, Columnas] = Acumulador / NumeroPersonas;
+                dataGridView.Rows[NumeroPersonas].Cells[Columnas + 1].Value = String.Format("{0:#,0.00}", MatrizPersonas[NumeroPersonas, Columnas]);
+                MatrizPersonas[(NumeroPersonas + 1), Columnas] = ValorMayor;
+                dataGridView.Rows[NumeroPersonas + 1].Cells[Columnas + 1].Value = String.Format("{0:#,0.00}", MatrizPersonas[NumeroPersonas +1 , Columnas]);
+                MatrizPersonas[(NumeroPersonas + 2), Columnas] = ValorMenor;
+                dataGridView.Rows[NumeroPersonas + 2].Cells[Columnas + 1].Value = String.Format("{0:#,0.00}", MatrizPersonas[NumeroPersonas + 2, Columnas]);
+                Acumulador = 0;
+            }
+                btnIngresarDatos.Enabled = true;
+                btnCalcularResultados.Enabled = false;
+
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+
+            DialogResult Salir = MessageBox.Show("Desea Salir (Si/No) ?", "Salir Aplicación", MessageBoxButtons.YesNo);
+            if (Salir == DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+
+
+
+    }
+
+    internal class Interaction
+    {
+        internal static object InputBox(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+
+        
+
+
+           
+
+
+      
+
+
+
